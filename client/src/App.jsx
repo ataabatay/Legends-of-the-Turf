5198f0811d42b50e22193071d90f01027b672c8d
aszab1 <adry_szabo@yahoo.co.uk>
2023-12-07 13:43:17 +0000

    client inital setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,24 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom'
 
+
+import Home from './components/Home'
+import Navigation from './components/Nav'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navigation />
+      <Home />
+    
+      <h1>Hello turds</h1>
+      <button>Login</button>
+      <button>Register</button>
+      {/* <main>
       <Outlet />
+      </main> */}
     </>
   )
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+export default function Home(){
+  return (
+    <>
+    
+    <section>Fixtures of the week</section>
+    
+    <section>League Table</section>
+    
+  
+    </>
+  )
+}
