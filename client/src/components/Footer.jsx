0a15af520f0049fe65acf3ec2b31dca7eed817b4
Ata Abatay <aliataabatay@gmail.com>
2023-12-11 19:32:52 +0000

    fixed footer link error now all link leading to correct github profiles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,14 +5,13 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <Link to='/your-route'
-        className='https://github.com/ataabatay'>
+        <Link to='https://github.com/ataabatay'>
           <p><img src={GitHub} style={{ width: '1.5vh' }}></img> Adrienn Szabo</p>
         </Link>
-        <Link to='' className='https://github.com/aszab1'>
+        <Link to='https://github.com/aszab1'>
           <p><img src={GitHub} style={{ width: '1.5vh' }}></img> Ata Abatay</p>
         </Link>
-        <Link to='' className='https://github.com/Jerrellbb'>
+        <Link to='https://github.com/Jerrellbb'>
           <p><img src={GitHub} style={{ width: '1.5vh' }}></img> Jerrell Boyer</p>
         </Link>
       </footer>
