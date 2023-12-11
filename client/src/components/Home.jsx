c5be93104b67ea76d802132d7b6426506266d7ab
aszab1 <adry_szabo@yahoo.co.uk>
2023-12-11 21:36:09 +0000

    cheched merges

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,21 @@
 import Fixtures from "./Fixtures";
 import LeagueTable from "./LeagueTable";
 import NewTeam from "./NewTeam";
-
+import { useLoaderData } from "react-router-dom";
 
 
 export default function Home(){
 
-
+const data = useLoaderData()
+const leagueTable = data.leagueTable.items[0].standings.tables[0].rows
+console.log(leagueTable)
   return (
     <>
+    <div className="container-wrapper">
     <Fixtures />
-    <LeagueTable />
-    <NewTeam />
+    <LeagueTable leagueTable = {leagueTable}/>
+    </div>
+    
     </>
   )
 }
--- client/src/components/LeagueTable.jsx
+++ client/src/components/LeagueTable.jsx
@@ -1,47 +1,59 @@
 import { useLoaderData } from 'react-router-dom'
 import Table from 'react-bootstrap/Table'
 import Container from 'react-bootstrap/Container'
+import green from '../assets/images/green.png'
+import draw from '../assets/images/draw.png'
+import red from '../assets/images/red.jpg'
 
-export default function LeagueTable(){
-  const leagueTable = useLoaderData()
+export default function LeagueTable({leagueTable}){
+  
+
+
 console.log(leagueTable)
 
 
-const tableContent = leagueTable.standings
-
 return (
-<Container>
+<Container className='table-container'> 
 <h2>Premier League Table</h2>
   <Table>
     <thead>
       <tr>
         <th>Position</th>
-        <th>Club</th>
-        <th>Played</th>
-        <th>Points</th>
-        <th>Won</th>
-        <th>Draw</th>
-        <th>Lost</th>
-        <th>Goal Difference</th>
-        <th>Recent form</th>
+        <th className='d-flex'>Club</th>
+        <th>MP</th>
+        <th>W</th>
+        <th>D</th>
+        <th>L</th>
+        <th>GF</th>
+        <th>GA</th>
+        <th>GD</th>
+        <th>Pts</th>
+        <th>Last 5</th>
       </tr>
     </thead>
     <tbody>
-      {tableContent?.standings?.[0].table.length
-      ? tableContent.standings[0].table.map(team => (
-        <tr key={team.team.id}>
-        <td>{team.position}</td>
-        <td>{team.team.name}</td>
-        <td>{team.playedGames}</td>
-        <td>{team.points}</td>
+      {leagueTable.map(team => (
+        <tr key={team.clubId}>
+        <td>{team.position}</td>  
+        <td className='d-flex'><img className='ltable-logo' src={team.crestUrl} alt={team.clubName} />{team.clubName}</td>
+        <td>{team.played}</td>
         <td>{team.won}</td>
-        <td>{team.draw}</td>
+        <td>{team.drawn}</td>
         <td>{team.lost}</td>
+        <td>{team.goalsFor}</td>
+        <td>{team.goalsAgainst}</td>
         <td>{team.goalDifference}</td>
+        <td>{team.points}</td>
+        <td>{team.recentForm.map(result => 
+          result === 'W' ? <img className='plt-logo' src={green} /> :
+          result === 'D' ? <img className='plt-logo' src={draw} /> :
+          result === 'L' ? <img className='plt-logo' src={red} /> :
+          result)}
+          </td>
         
         </tr>
-      ))
-    : <tr><td>No data</td></tr>}
+      ))}
+    
     </tbody>
   </Table>
 
