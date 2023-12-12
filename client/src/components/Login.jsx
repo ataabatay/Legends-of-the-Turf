1a11748a0bf76bf0b1bd2cfc021b22e3e39cbab0
aszab1 <adry_szabo@yahoo.co.uk>
2023-12-12 23:17:11 +0000

    designed forms

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,30 +1,36 @@
 import { useEffect } from 'react'
 import { Form, useActionData, useNavigate } from 'react-router-dom'
-import { setToken } from '../../utils/helpers/common' 
+import { setToken } from '../../utils/helpers/common'
 
 
 
-export default function Login(){
+export default function Login() {
   const res = useActionData()
   const navigate = useNavigate()
-  
+
   useEffect(() => {
-    if (res?.status === 202){
+    if (res?.status === 202) {
       setToken(res.data.token)
       navigate('/Home')
     }
   }, [res, navigate])
   return (
-    <>
-      <h1 className="text-center bold display-3 mb-4">Login</h1>
-      <Form className='form' method="POST">
-        <input type="email" name="email" placeholder='Email' />
-        <input type="password" name="password" placeholder="Password" />
-        <button className='btn btn-blue' type="submit">Login</button>
-        {res && <p className='danger'>{res.data.message}</p>}
-      </Form>
-    </>
-  )
+    
+      
+    <div className='login-container'>
+    <Form className="login-form" method="POST">
+    <p class="login-title display-6 mb-8">Login</p>
+      <div class="form-group">
+        <input type="email" class="form-control" id="email" name="email" placeholder='Email' placeholder="Your email" />
+        </div>
+        <div class="form-group">
+        <input className="input" type="password" class="form-control" id="password" name="password" placeholder="Password" />
+        <button class='btn btn-outline-primary btn-md m-4' type="submit">Login</button>
+        {res && <p class='danger'>{res.data.message}</p>}
+        </div >
+    </Form>
+    </div>
+
+)
 }
 
-    
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -11,18 +11,30 @@ export default function Register(){
     }
   }, [res, navigate])
   return (
-    <>
-    <h1 className="text-center bold display-3 mb-4">Register</h1>
-      <Form className='form' method="POST">
+    <div className="register-container">
+      <Form  className='form' method="POST">
+      <h1 className="text-center bold display-6 mb-4">Register</h1>
+      <div class="input-group">
         <input type="text" name="first-name" placeholder='First Name' />
+        </div>
+        <div class="input-group">
         <input type="text" name="last-name" placeholder='Last Name' />
+        </div>
+        <div class="input-group">
         <input type="text" name="username" placeholder='Username' />
+        </div>
+        <div class="input-group">
         <input type="email" name="email" placeholder='Email' />
+        </div>
+        <div class="input-group">
         <input type="password" name="password" placeholder='Password' />
+        </div>
+        <div class="input-group">
         <input type="password" name="passwordConfirmation" placeholder='Confirm password' />
-        <button className='btn btn-blue' type="submit">Register</button>
+        </div>
+        <button class='btn btn-outline-primary btn-md m-4'  type="submit">Register</button>
         {res && <p className='danger'>{res.data.message}</p>}
       </Form>
-    </>
+    </div>
   )
 }
