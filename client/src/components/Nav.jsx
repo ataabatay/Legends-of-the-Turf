0d0535c1501141c09dcc01287c703e58b4b8f2d8
Jerrell Boyer <jerrellbb@yahoo.co.uk>
2023-12-14 14:17:23 +0000

    merge conflicts

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -64,8 +64,9 @@ export default function Navigation() {
 
   return (
     <>
+    <Badgelinks />
+    {!(isLoginPage || isRegisterPage) && (
     <Navbar className="navbar" bg="primary" data-bs-theme="dark">
-      
       <Container fluid style={{ paddingLeft: 0}}>
         <Nav className="me-auto">
           <Nav.Link onClick={handleClick}><button type='button' className='btn btn-primary' id='/home'>Home</button></Nav.Link>
