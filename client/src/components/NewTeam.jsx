70123e71cf72c914bd8c99cdda05095459be9474
Ata Abatay <aliataabatay@gmail.com>
2023-12-14 11:52:54 +0000

    image upload fixed

--- client/src/components/NewTeam.jsx
+++ client/src/components/NewTeam.jsx
@@ -34,9 +34,8 @@ export default function NewTeam() {
       <h1>Create Your Team</h1>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column', padding: '20px' }}>
         <Form method="POST" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', padding: '20px', maxWidth: '400px' }}>
+        <ImageUploadField value={formData.image} formData={formData} setFormData={setFormData} />
           <input type="text" name="teamName" placeholder='Team Name' onChange={handleChange} value={formData.teamName} style={{ margin: '5px', padding: '10px', width: '100%', boxSizing: 'border-box' }} />
-          {/* <ImageUploadField setFormData={setFormData} formData={formData} /> */}
-          <ImageUploadField value={formData.image} onChange={handleChange} formData={formData} setFormData={setFormData} />
           <input type="submit" value="Create Team" style={{ margin: '10px', padding: '10px', cursor: 'pointer', backgroundColor: 'royalblue', color: 'white', border: 'none', borderRadius: '5px' }} />
         </Form>
       </div>
