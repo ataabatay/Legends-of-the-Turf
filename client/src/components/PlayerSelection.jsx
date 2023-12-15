f0de4dd903590c5301b6d557e344d4a1ab9a54dd
Ata Abatay <aliataabatay@gmail.com>
2023-12-15 09:44:09 +0000

    minor timeout introduction to delay the navigation to myteam to load the team properly

--- client/src/components/PlayerSelection.jsx
+++ client/src/components/PlayerSelection.jsx
@@ -44,7 +44,7 @@ export default function PlayerSelection() {
     const teamId = await getCurrentUserTeamId()
     setTimeout(()=> {
       navigate(`/myteam/${teamId}`)
-    }, 1)
+    }, 200)
   }
 
   return (
